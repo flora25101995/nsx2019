b421eb6788228fd6de66f74d494cd782613df765
flora25101995 <flora25101995@gmail.com>
2019-05-20 20:51:30 +0700

    add css

--- index.js
+++ index.js
@@ -2,18 +2,86 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.use(express.static(__dirname + '/assets'))
 app.set('view engine', 'ejs')
 
 app.get('/api/getlist', (req, res) => {
-    res.json(['a', 'b', 'c'])
+    res.json({
+        user: 
+        [
+            {
+                name: 'Quyen',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Dung',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Thuan',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Mai',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Nhi',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'An',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Linh',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Do',
+                class: 'PQ-WEB-D002'
+            },
+        ]
+    })
 })
 
 app.get('/', (req, res) => {
     res.render('index', {
-        user: {
-            name: 'Quyen'
-        },
-        users: ['apple', 'samsung', 'huawei']
+        users: 
+        [
+            {
+                name: 'Quyen',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Dung',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Thuan',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Mai',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Nhi',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'An',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Linh',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Do',
+                class: 'PQ-WEB-D002'
+            },
+        ]
     })
 })
 
